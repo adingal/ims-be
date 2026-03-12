72e666976df67d0b035ad983a53e8ec34acdeebc
Lhex <xelalagnid@gmail.com>
2026-03-12 18:06:44 +0800

    Setup local db connection

---
+++ server.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+
+// Insert config
+dotenv.config({ path: './config.env' })
+const app = require('./app')
+
+// Connect to DB
+mongoose
+  .connect(process.env.DATABASE_LOCAL)
+  .then(() => console.log('DB connection successful.'))
+  .catch((err) => console.log('DB connection fail.', err))
+
+const port = process.env.PORT || 3000
+const server = app.listen(port, () => {
+  console.log(`App running on port: ${port}...`)
+})
